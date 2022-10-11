2a8edebfa01bc3b7999da2058edee6b9e0d412ae
Vincent Young <u@nisekoo.com>
2022-10-12 05:42:35 +0800

    Modify version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 Author: Vincent Young
 Date: 2022-07-24 04:27:43
 LastEditors: Vincent Young
-LastEditTime: 2022-10-12 05:10:48
+LastEditTime: 2022-10-12 05:40:08
 FilePath: /GmailValidChecker/setup.py
 Telegram: https://t.me/missuo
 
@@ -16,7 +16,7 @@ with open("README.md","r") as fh:
 setup(
     name="GmailChecker",
     author="missuo",
-    version="0.0.1",
+    version="0.0.2",
     license='MIT',
     long_description= long_description,
     long_description_content_type="text/markdown",
